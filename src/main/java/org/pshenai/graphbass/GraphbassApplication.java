e4b2765a249265ae0fe52d7368e4f9ec10e78c6e
pshenai <antonvoloshyn25@gmail.com>
2024-04-01 12:45:18 +0300

    Initial commit.

---
+++ src/main/java/org/pshenai/graphbass/GraphbassApplication.java
@@ -0,0 +1,13 @@
+package org.pshenai.graphbass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphbassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphbassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/pshenai/graphbass/GraphbassApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pshenai.graphbass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphbassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
